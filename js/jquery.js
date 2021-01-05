dc1a4161d6b25b429a9d0b29e559048d52b6bc40
AdminCabbage <1119866279@qq.com>
2021-01-05 15:52:34 +0800

    Site updated: 2021-01-05 15:52:31

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>var pass1 = prompt('请输入密码','');  
+if (pass1 == "NbFeSEcSbbB69sBSf69sBHh") { 
+alert('密码正确!');    
+}else{
+alert('密码错误宝贝.');
+window.location.href="https://www.tu3k.cn"; 
+} 
